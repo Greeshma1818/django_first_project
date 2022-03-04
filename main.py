a65ed9702a1aafdf78557545331bbba979fe781b
Greeshma1818 <100897322+Greeshma1818@users.noreply.github.com>
2022-03-04 11:02:39 +0530

    sample file testing

---
+++ main.py
@@ -0,0 +1,11 @@
+#http://127.0.0.1:8000/
+
+
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+def hello(request):
+    return HttpResponse("<h2>Hello, Welcome to Django!</h2>")
